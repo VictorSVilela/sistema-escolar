0f82a090c4b1b8f2784fa69f10fb76879afd8a4b
Victor Vilela <victor_svg@yahoo.com.br>
2021-05-19 17:11:53 -0300

    Remodelando e configurando Alunos

--- client/aluno/js/controllers/aluno-incluir.controller.js
+++ client/aluno/js/controllers/aluno-incluir.controller.js
@@ -6,13 +6,24 @@ function AlunoIncluirController($scope, AlunoService) {
 
     $scope.adicionarAluno = adicionarAluno;
 
+    _inicializar();
+
     ////////////////////////////////////////////////
+
+    function _inicializar() {
+        listar();
+    }
+
+    function listar(){
+        AlunoService.getALunos().then(response => {
+            $scope.alunos = response.data;
+        });
+    }
 
     function adicionarAluno(aluno) {
         AlunoService.adicionarAluno(aluno).then(response => {
             $scope.alunos = response.data;
-            delete $scope.aluno;
-            $scope.alunoForm.$setPristine();
+            listar();
         });
     }
 }
