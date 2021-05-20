81fedf1026076b9c92a58608daccfe875b4b946c
Victor Vilela <victor_svg@yahoo.com.br>
2021-05-20 11:07:31 -0300

    Criando UsuarioController Backend

--- server/src/main/java/br/com/grupoitss/controllers/UsuarioController.java
+++ server/src/main/java/br/com/grupoitss/controllers/UsuarioController.java
@@ -2,10 +2,7 @@ package br.com.grupoitss.controllers;
 
 import br.com.grupoitss.model.Usuario;
 
-import javax.ws.rs.Consumes;
-import javax.ws.rs.POST;
-import javax.ws.rs.Path;
-import javax.ws.rs.Produces;
+import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import java.util.HashMap;
@@ -26,9 +23,43 @@ public class UsuarioController {
         return usuario;
     }
 
-    public Response consultar(Long id) {
-        return null;
+    @GET
+    @Path("/{id}")
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response consultar(@PathParam("id") Long id) {
+        if(usuarios.containsKey(id)) {
+            Usuario usuario = usuarios.get(id);
+            return Response.ok(usuario).build();
+        }
+        return Response.status(Response.Status.NOT_FOUND).build();
     }
 
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response listarTodos() {
+        return Response.ok(usuarios.values()).build();
+    }
 
+    @PUT
+    @Path("/{id}")
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response alterar(@PathParam("id") Long id, Usuario usuario) {
+        if(usuarios.containsKey(id)){
+            usuario.setId(id);
+            usuarios.put(id, usuario);
+            return Response.ok(usuarios.get(id)).build();
+        }
+        return Response.status(Response.Status.NOT_FOUND).build();
+    }
+
+    @DELETE
+    @Path("/{id}")
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response deletar(@PathParam("id") Long id) {
+        if (usuarios.containsKey(id)){
+            usuarios.remove(id);
+            return Response.ok().build();
+        }
+        return Response.status(Response.Status.NOT_FOUND).build();
+    }
 }
