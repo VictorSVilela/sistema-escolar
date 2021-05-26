f8c1311ef96125f91240e6cc067c65b8e72365bf
Victor Vilela <victor_svg@yahoo.com.br>
2021-05-26 20:58:24 -0300

    Criando entidade e controller Matéria

---
+++ server/src/main/java/br/com/grupoitss/controllers/MateriaController.java
@@ -0,0 +1,66 @@
+package br.com.grupoitss.controllers;
+
+import br.com.grupoitss.model.Escola;
+import br.com.grupoitss.model.Materia;
+
+import javax.ws.rs.*;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import java.util.HashMap;
+import java.util.Map;
+
+@Path("/materias")
+public class MateriaController {
+
+    private static final Map<Long, Materia> materias = new HashMap<>();
+    private static Long idGerador = 1L;
+
+    @POST
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.APPLICATION_JSON)
+    public Materia incluir(Materia materia) {
+        materia.setId(idGerador++);
+        materias.put(materia.getId(), materia);
+        return materia;
+    }
+
+    @GET
+    @Path("/{id}")
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response consultar(@PathParam("id") Long id) {
+        if (materias.containsKey(id)) {
+            Materia materia = materias.get(id);
+            return Response.ok(materia).build();
+        }
+        return Response.status(Response.Status.NOT_FOUND).build();
+    }
+
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response listarTodos() {
+        return Response.ok(materias.values()).build();
+    }
+
+    @PUT
+    @Path("/{id}")
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response alterar(@PathParam("id") Long id, Materia materia) {
+        if (materias.containsKey(id)) {
+            materia.setId(id);
+            materias.put(id, materia);
+            return Response.ok(materias.get(id)).build();
+        }
+        return Response.status(Response.Status.NOT_FOUND).build();
+    }
+
+    @DELETE
+    @Path("/{id}")
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response deletar(@PathParam("id") Long id) {
+        if (materias.containsKey(id)) {
+            materias.remove(id);
+            return Response.ok().build();
+        }
+        return Response.status(Response.Status.NOT_FOUND).build();
+    }
+}
---
+++ server/src/main/java/br/com/grupoitss/model/Materia.java
@@ -0,0 +1,61 @@
+package br.com.grupoitss.model;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "tb_materia")
+public class Materia {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String nome;
+
+    @OneToOne
+    private Usuario professor;
+
+    private String descricao;
+
+    public Materia() {
+    }
+
+    public Materia(Long id, String nome, Usuario professor, String descricao) {
+        this.id = id;
+        this.nome = nome;
+        this.professor = professor;
+        this.descricao = descricao;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Usuario getProfessor() {
+        return professor;
+    }
+
+    public void setProfessor(Usuario professor) {
+        this.professor = professor;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+}
